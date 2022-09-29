78613b65fed765e9926526e8aeddf68bd6853fc0
Abduino <abdrehmanredi0@gmail.com>
2022-09-29 16:35:34 -0300

    create matrix using array

---
+++ matrix.m
@@ -0,0 +1,25 @@
+# Created by Octave 4.4.1, Thu Jun 23 14:36:57 2022 GMT <unknown@DESKTOP-0D3AF2C>
+# name: A
+# type: matrix
+# rows: 5
+# columns: 5
+ 1 1 1 1 1
+ 1 1 1 1 1
+ 1 1 1 1 1
+ 1 1 1 1 1
+ 1 1 1 1 1
+
+
+# name: B
+# type: matrix
+# rows: 2
+# columns: 4
+ 1 2 3 4
+ 5 6 7 8
+
+
+# name: ans
+# type: scalar
+4
+
+
