ce5ec88462b151be97d3bd6c660b552c466d33c2
Abduino <abdrehmanredi0@gmail.com>
2022-09-29 16:29:04 -0300

    initialization

---
+++ ifelse.m
@@ -0,0 +1,14 @@
+a = 100;
+% check the boolean condition 
+ if a < 20 
+    % if condition is true then print the following 
+    fprintf('a is less than 20\n' );
+ else
+    % if condition is false then print the following 
+    fprintf('a is not less than 20\n' );
+ end
+ fprintf('value of a is : %d\n', a);
+ 
+ for a = 10:20 
+   fprintf('value of a: %d\n', a);
+end
